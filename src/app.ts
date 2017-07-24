c79b03e773bd5562e0425d41e0e936008fd7d46f
Karl Kong <karlkong2004@hotmail.com>
2017-07-25 02:50:15 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.get('/health', function (req, res) {
 
 app.use(new MorganWrapper("TripPlanner").logger);
 // app.use(bodyParser.json({}));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/', tripPlanner);
